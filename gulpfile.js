9ad100b6b21597e0f674b9453f07df99286a60cd
glennphilp <glennphilp@hotmail.com>
2014-07-09 15:22:14 -0500

    Added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
 // include gulp
-var gulp    = require('gulp'),
-    util    = require('gulp-util'),
-    coffee  = require('gulp-coffee'),
-    concat  = require('gulp-concat');
+var gulp        = require('gulp'),
+    util        = require('gulp-util'),
+    browserify  = require('gulp-browserify'),
+    coffee      = require('gulp-coffee'),
+    concat      = require('gulp-concat');
 
 var paths = {
-  coffeeSrc: ['./src/components/coffee/tagline.coffee'],
-  coffeeDst: ['./src/components/js'],
-  jsSrc: [
+  coffeeSrc:  ['src/components/coffee/tagline.coffee'],
+  coffeeDst:  'src/components/js',
+  jsSrc:      [
     'src/components/js/rclick.js',
-    'src/components/js/pixgrid.js'
+    'src/components/js/pixgrid.js',
+    'src/components/js/tagline.js',
+    'src/components/js/template.js'
   ],
-  jsDevDst: 'src/js/'
+  jsDevDst:   'src/js'
 //    imgSrc: './src/images/**/*',
 //    imgDst: './build/images',
 //    htmlSrc: './src/*.html',
@@ -23,19 +26,16 @@ var paths = {
 };
 
 
-// Preprocessing Coffee Script
-// FIXME: Revisit why Coffee is processing since
-// changing src and dest to the path variables instead
-// of the inline paths.
 gulp.task('coffee', function() {
   gulp.src(paths.coffeeSrc)
     .pipe(coffee({ bare: true })
       .on('error', util.log))
-    .pipe(gulp.dest(paths.coffeeDst));
+    .pipe(gulp.dest(paths.coffeeDst))
 });
 
 gulp.task('js', function() {
   gulp.src(paths.jsSrc)
     .pipe(concat('script.js'))
-    .pipe(gulp.dest(paths.jsDevDst));
+    .pipe(browserify())
+    .pipe(gulp.dest(paths.jsDevDst))
 });
