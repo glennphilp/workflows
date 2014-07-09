54e2796e0f63b43a8ecb5d4207b28bbca10e81ea
glennphilp <glennphilp@hotmail.com>
2014-07-09 10:58:19 -0500

    Installed gulp

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+// include gulp
+var gulp = require('gulp'); 
+ 
+// include plug-ins
+//var jshint = require('gulp-jshint'),
+//    changed = require('gulp-changed'),
+//    imagemin = require('gulp-imagemin'),
+//    minifyHTML = require('gulp-minify-html'),
+//    concat = require('gulp-concat'),
+//    stripDebug = require('gulp-strip-debug'),
+//    uglify = require('gulp-uglify'),
+//    sass = require('gulp-sass'),
+//    compass = require('gulp-compass'),
+//    autoprefix = require('gulp-autoprefixer'),
+//    minifyCSS = require('gulp-minify-css');
+
+//var paths = {
+//    jsSrc: ['./src/js/foundation.js', './src/js/foundation/*.js'],
+//    jsDst: './build/js',
+//    imgSrc: './src/images/**/*',
+//    imgDst: './build/images',
+//    htmlSrc: './src/*.html',
+//    htmlDst: './build',
+//    scssSrc: ['./src/scss/**/*.scss', '!./src/scss/core/**/*.scss'],
+//    scssDst: './src/css/',
+//    cssSrc: './src/css/**/*.css',
+//    cssDst: './build/css'
+};
